8ce0ba00f49c01433e1988e9a2df55e19ab13377
iqbal-neT35 <muhammadiqbal13493@gmail.com>
2025-01-06 18:52:23 +0700

    revisi bagian database yang tidak perlu

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -20,7 +20,6 @@ class Agenda extends Model
         'tempat',
         'list_daftar_nama',
         'report',
-        'priority_number',
     ];
 
     public function users()
@@ -31,30 +30,7 @@ class Agenda extends Model
     protected $casts = [
         'list_daftar_nama' => 'array',
     ];
-    // number prioritas untuk logika tabel
-    public static function generatePriorityNumber($kategori)
-    {
-        // Tentukan prefix berdasarkan kategori
-        $prefix = match ($kategori) {
-            'Biasa' => 'B',
-            'Penting' => 'P',
-            'Rapat' => 'R',
-            default => 'X', // Kategori tidak dikenal
-        };
-
-        // Ambil nomor terakhir dari kategori yang sama
-        $lastPriority = self::where('priority_number', 'like', "$prefix%")
-            ->orderBy('priority_number', 'desc')
-            ->pluck('priority_number')
-            ->first();
-
-        // Ekstrak angka dari nomor terakhir
-        $nextNumber = $lastPriority ? ((int) substr($lastPriority, 1)) + 1 : 1;
-
-        // Format nomor prioritas baru (contoh: P001)
-        return $prefix . str_pad($nextNumber, 3, '0', STR_PAD_LEFT);
-    }
-
+    
     public function bidang()
     {
         return $this->belongsTo(Bidang::class);
