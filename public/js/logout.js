0ceda60dba163f71cdb091c992c16e4b37b2ec3d
iqbal-neT35 <muhammadiqbal13493@gmail.com>
2025-01-11 22:44:53 +0700

    commit first

---
+++ public/js/logout.js
@@ -0,0 +1,4 @@
+     document.getElementById('logoutButton').addEventListener('click', function(event) {
+                    event.preventDefault();
+                    document.getElementById('logoutForm').submit();
+                });
